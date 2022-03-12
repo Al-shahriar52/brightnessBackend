46762958947a8384bf26399219db102db49f0eaa
Al-shahriar52 <shahriar15-2646@diu.edu.bd>
2022-03-12 13:11:00 +0600

    last2

--- base/views.py
+++ base/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Product
 from .products import products
+from .serailizers import ProductSerializer
 # Create your views here.
 
 @api_view(['GET'])
@@ -20,16 +22,13 @@ def getRouts(request):
     return Response(routes)
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products)
+    products=Product.objects.all()
+    serailizer=ProductSerializer(products,many=True)
+    return Response(serailizer.data)
 
 @api_view(['GET'])
 def getProduct(request ,pk):
-    product = None
-    
-    for i in products:
-        if i['_id']==pk:
-            product=i
-            
-            break
+    product= Product.objects.get(_id=pk)
+    serailizer=ProductSerializer(product,many=False)
 
-    return Response(product)
+    return Response(serailizer.data)
